7ef8c61c0c31a583422a352c2ad4c329863a40dd
paresiqbal <thecrowdel@gmail.com>
2025-02-18 10:00:21 +0700

    asdas

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
               mudah, aman, dan efisien.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
-              <Link href="/student">
+              <Link href="/landing">
                 <Button className="w-full sm:w-auto">
                   Dashboard Siswa
                   <ArrowRightCircle className="ml-2 h-5 w-5" />
